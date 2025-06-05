22ba3c49ab3c20f3d772435c3eaaf014f3c1a0cf
MIKEWILL <triple888devs@gmail.com>
2025-06-05 22:43:45 +0300

    FANVERSE-Where Storytellers are made

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "http://localhost:3001/supabase";
+const SUPABASE_URL = "https://bhgwipcwsndpyvrzcpma.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoZ3dpcGN3c25kcHl2cnpjcG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxOTI5NTMsImV4cCI6MjA1MTc2ODk1M30.zWVIQ0byaasTeVdNMRxwY47UczGS5O7pJxGmvu5u3ew";
 
 const customFetch = async (input: RequestInfo, init?: RequestInit) => {
